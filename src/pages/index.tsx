2db36537ef9be8b419c1b002c71dc76f34cd3a70
Matt <matt@elixxir.io>
2023-11-07 06:56:06 +0700

    Remove unused space

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,6 @@ export default function Home() {
                     </td>
                   </tr>
                 ))}
-                
               </tbody>
             </table>
           )}
